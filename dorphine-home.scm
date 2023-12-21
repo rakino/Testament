5d4da5c4f5dc0638bc0cb17602a6793259ee9d76
Hilton Chain <hako@ultrarare.space>
2023-12-21 13:39:32 +0800

    emacs: Move font configuration to a separate file.
    * files/emacs-init-font.el: New file.
    * dorphine-home.scm (home-emacs-configuration)[extra-init-files]: Add it
    [extra-init]: Remove field.
    (%emacs-extra-init): Delete variable.

--- dorphine-home.scm
+++ dorphine-home.scm
@@ -173,66 +173,6 @@ fi")))
                                 (or (getenv "XDG_DATA_HOME")
                                     "~/.local/share")))))))
 
-;; Copied from <https://github.com/nykma/nema/blob/develop/my-sample/font.el>
-(define %emacs-extra-init
-  (list #%(defvar nema-fonts '((default . "Victor Mono")
-                               (unicode . "LXGW WenKai TC")
-                               (modeline . "Source Serif Pro")
-                               (fixed . "Sarasa Mono TC")
-                               (variable . "Bembo Std"))
-            "Fonts to use.")
-        #%(defvar nema--font-size 12 "Font size")
-
-        #%(defun nema//get-font-family (key)
-            (alist-get key nema-fonts))
-
-        #%(defun nema//generate-font-spec (key)
-            (format "%s-%d"
-                    (nema//get-font-family key)
-                    nema--font-size))
-
-        #%(defun nema//load-base-font ()
-            "Load the default font for ascii characters."
-            (let* ((font-spec (nema//generate-font-spec 'default)))
-              (set-frame-parameter nil 'font font-spec)
-              (add-to-list 'default-frame-alist (cons 'font font-spec))))
-
-        #%(defun nema//load-face-font ()
-            "Load fonts used in faces.
-
-                This function must be called after frame creation."
-            (let ((modeline-font-spec (nema//generate-font-spec 'modeline))
-                  (variable-font-spec (nema//generate-font-spec 'variable))
-                  (fixed-font-spec (nema//generate-font-spec 'fixed)))
-              (set-face-attribute 'variable-pitch nil :font variable-font-spec :height 1.1)
-              (set-face-attribute 'fixed-pitch nil :font fixed-font-spec)
-              (set-face-attribute 'fixed-pitch-serif nil :font fixed-font-spec)
-              (set-face-attribute 'mode-line nil :font modeline-font-spec)
-              (set-face-attribute 'mode-line-inactive nil :font modeline-font-spec)
-              (set-face-attribute 'tab-bar nil :font modeline-font-spec)))
-
-        #%(defun nema//load-ext-font ()
-            "Load fonts used for non-ascii characters.
-
-                This function must be called after frame creation."
-            (let ((font (frame-parameter nil 'font))
-                  (font-spec (font-spec :family (nema//get-font-family 'unicode))))
-              (dolist (charset '(kana han hangul cjk-misc bopomofo symbol))
-                      (set-fontset-font font charset font-spec))))
-
-        #%(defun nema/load-font()
-            "Load all font configuration."
-            (interactive)
-            (when (display-graphic-p)
-              (nema//load-base-font)
-              (nema//load-ext-font)
-              (nema//load-face-font)))
-
-        #%(if (daemonp)
-              (add-hook 'server-after-make-frame-hook #'nema/load-font)
-              ;; Else: not in daemon
-              (add-hook 'after-init-hook #'nema/load-font))))
-
 (define %emacs-set-org-capture-template
   (list #%(defun org-hugo-new-subtree-post-capture-template ()
             "Returns `org-capture' template string for new Hugo post.
@@ -377,6 +317,9 @@ fi")))
                   (default-init
                     (emacs-configuration
                      (early-init %emacs-early-init)
+                     (extra-init-files
+                      `(("font.el"
+                         . ,(testament-file-object "emacs-init-font.el"))))
                      (variables
                       '((create-lockfiles . #f)
                         (enable-recursive-minibuffers . #t)
@@ -409,8 +352,7 @@ fi")))
                         (show-paren-mode . #t)
                         (tool-bar-mode . #f)
                         (tooltip-mode . #f)))
-                     (keys '(("C-x C-b" . switch-to-buffer)))
-                     (extra-init %emacs-extra-init)))
+                     (keys '(("C-x C-b" . switch-to-buffer)))))
                   (configured-packages
                    (list
                     (emacs-package
---
+++ files/emacs-init-font.el
@@ -0,0 +1,59 @@
+;; Copied from <https://github.com/nykma/nema/blob/develop/my-sample/font.el>
+
+(defvar nema--font-size 12 "Font size")
+(defvar nema-fonts '((default . "Victor Mono")
+                     (unicode . "LXGW WenKai TC")
+                     (modeline . "Source Serif Pro")
+                     (fixed . "Sarasa Mono TC")
+                     (variable . "Bembo Std"))
+  "Fonts to use.")
+
+(defun nema//get-font-family (key)
+  (alist-get key nema-fonts))
+
+(defun nema//generate-font-spec (key)
+  (format "%s-%d"
+          (nema//get-font-family key)
+          nema--font-size))
+
+(defun nema//load-base-font ()
+  "Load the default font for ascii characters."
+  (let* ((font-spec (nema//generate-font-spec 'default)))
+    (set-frame-parameter nil 'font font-spec)
+    (add-to-list 'default-frame-alist (cons 'font font-spec))))
+
+(defun nema//load-face-font ()
+  "Load fonts used in faces.
+
+This function must be called after frame creation."
+  (let ((modeline-font-spec (nema//generate-font-spec 'modeline))
+        (variable-font-spec (nema//generate-font-spec 'variable))
+        (fixed-font-spec (nema//generate-font-spec 'fixed)))
+    (set-face-attribute 'variable-pitch nil :font variable-font-spec :height 1.1)
+    (set-face-attribute 'fixed-pitch nil :font fixed-font-spec)
+    (set-face-attribute 'fixed-pitch-serif nil :font fixed-font-spec)
+    (set-face-attribute 'mode-line nil :font modeline-font-spec)
+    (set-face-attribute 'mode-line-inactive nil :font modeline-font-spec)
+    (set-face-attribute 'tab-bar nil :font modeline-font-spec)))
+
+(defun nema//load-ext-font ()
+  "Load fonts used for non-ascii characters.
+
+This function must be called after frame creation."
+  (let ((font (frame-parameter nil 'font))
+        (font-spec (font-spec :family (nema//get-font-family 'unicode))))
+    (dolist (charset '(kana han hangul cjk-misc bopomofo symbol))
+      (set-fontset-font font charset font-spec))))
+
+(defun nema/load-font()
+  "Load all font configuration."
+  (interactive)
+  (when (display-graphic-p)
+    (nema//load-base-font)
+    (nema//load-ext-font)
+    (nema//load-face-font)))
+
+(if (daemonp)
+    (add-hook 'server-after-make-frame-hook #'nema/load-font)
+  ;; Else: not in daemon
+  (add-hook 'after-init-hook #'nema/load-font))
