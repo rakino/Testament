00efb8c4ddc57305304577f6d853d96eb980cf86
Hilton Chain <hako@ultrarare.space>
2024-12-10 01:17:03 +0800

    dorphine: Use default Shepherd.
    Shepherd 1.0.0 is released.
    
    * src/modules/testament/packages.scm (shepherd-1.0.0rc2): Delete variable.
    * cfg/dorphine.org (Guix System,Guix Home): Remove it.

--- src/modules/testament/packages.scm
+++ src/modules/testament/packages.scm
@@ -15,7 +15,6 @@
   #:use-module (guix build-system emacs)
   #:use-module (guix build-system pyproject)
 
-  #:use-module (gnu packages admin)
   #:use-module (gnu packages android)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages chromium)
@@ -186,18 +185,6 @@ counterpart is unavailable.")
      (append
       (package-propagated-inputs mpv)
       (package-inputs mpv)))))
-
-(define-public shepherd-1.0.0rc2
-  (package
-    (inherit shepherd)
-    (version "1.0.0rc2")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://alpha.gnu.org/gnu/shepherd/shepherd-"
-                                  version ".tar.gz"))
-              (sha256
-               (base32
-                "1nr4aifixcqn6nvlc0hlf9n9k687835w0ngzn5crpgb3rsl9nv1i"))))))
 
 (define-public steam-nvidia/dolly
   (hidden-package
