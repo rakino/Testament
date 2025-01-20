e7cd170e2aebc77d7b6e06d197da044084514e7d
Hilton Chain <hako@ultrarare.space>
2025-01-20 19:29:52 +0800

    dorphine: Use fish shell.
    * src/modules/testament/packages.scm (emacs-eat/dolly): New variable.
    * cfg/dorphine.org (Guix System)[users]: Change shell.
    (greetd): Change shell.
    (Guix Home): Add fish configuration.
    (atuin,direnv,emacs-eat,sway,zoxide): Adjust for fish.
    (bash,blesh,liquidprompt): Remove sections.

--- src/modules/testament/packages.scm
+++ src/modules/testament/packages.scm
@@ -60,6 +60,16 @@
     (description "")
     (license license:asl2.0)))
 
+(define-public emacs-eat/dolly
+  (hidden-package
+   (package-with-extra-patches emacs-eat
+     (list (origin
+             (method url-fetch)
+             (uri "https://codeberg.org/akib/emacs-eat/pulls/133.patch")
+             (sha256
+              (base32
+               "18kjwmpz88i3f7dm33hkqcmabdd73wyzvm2w1h451wf4zlwcnp37")))))))
+
 (define-public emacs-eglot-booster/dolly
   (hidden-package
    (package
