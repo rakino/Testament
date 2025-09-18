3077ccb0d7827098005e25cfedcf8874d53b775e
Hilton Chain <hako@ultrarare.space>
2025-09-19 01:07:01 +0800

    Rewrite configurations, part 4.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: CC0-1.0
 
 ARGS  :=
-OPTS  := --max-jobs=1 --cores=32 --keep-going --verbosity=1 $(ARGS)
+OPTS  := --keep-going --verbosity=1 $(ARGS)
 GUIX  := guix time-machine --channels=channels.lock --
 EMACS := $(GUIX) shell emacs-minimal -- emacs
 
--- common.scm
+++ common.scm
@@ -49,21 +49,12 @@
         %guix-authorized-key-bocis
         %guix-authorized-key-ignamma))
 
-;; Managed by Zheng Junjie.
-(define %guix-authorized-key-sin
-  (plain-file "sin.pub" "
-(public-key
- (ecc
-  (curve Ed25519)
-  (q #CC7E2CD7CA12B6CB9A822C0BFD14E2D7347FF6B96CB1D796513A813F07475B20#)))"))
-
 (define %hako-guix-authorized-keys-head
   (list %guix-authorized-key-dorphine
         %guix-authorized-key-gokuraku
 
         %guix-authorized-key-bocis
-        %guix-authorized-key-ignamma
-        %guix-authorized-key-sin))
+        %guix-authorized-key-ignamma))
 
 
 (define %ssh-key-deploy
@@ -181,126 +172,3 @@ WARNED."
   (local-file (testament-blobs "gokuraku.yaml")))
 (define rakuen.yaml
   (local-file (testament-blobs "rakuen.yaml")))
-
-
-(define (sing-box-config)
-  (define %rule-sets
-    (let ((geosite
-           (lambda (rule-set)
-             `(("type" . "remote")
-               ("tag" . ,(format #f "geosite-~a" rule-set))
-               ("url" . ,(format #f "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/sing/geo/geosite/~a.srs" rule-set))
-               ("download_detour" . "OUT: Proxy"))))
-          (geoip
-           (lambda (rule-set)
-             `(("type" . "remote")
-               ("tag" . ,(format #f "geoip-~a" rule-set))
-               ("url" . ,(format #f "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/sing/geo/geoip/~a.srs" rule-set))
-               ("download_detour" . "OUT: Proxy")))))
-      (append
-       (map geosite
-            '("category-ads-all"
-              "category-dev"
-              "cn"
-              "gfw"
-              "private"
-              "stripe"))
-       (map geoip
-            '("cn"
-              "telegram")))))
-
-  (define %block-rules
-    '((("rule_set" . "geosite-category-ads-all"))))
-
-  (define %direct-rules
-    '((("protocol" . "bittorrent"))
-      (("rule_set" . "geosite-private"))
-      (("rule_set" . "geosite-cn"))
-      (("rule_set" . "geoip-cn"))))
-
-  (define %proxy-rules
-    '((("rule_set" . "geosite-gfw"))
-      (("rule_set" . "geosite-category-dev"))
-      (("rule_set" . "geosite-stripe"))
-      (("domain_suffix" . "boiledscript.com"))
-      (("domain_suffix" . "freedesktop.org"))
-      (("rule_set" . "geoip-telegram"))
-      (("inbound" . "IN: Proxy"))))
-
-  (define %config
-    `(("log"
-       ("level" . "warn"))
-      ("dns"
-       ("servers"
-        . #((("type" . "tls")
-             ("tag" . "DNS: Direct")
-             ("server" . "223.5.5.5"))
-            (("type" . "tls")
-             ("tag" . "DNS: Proxy")
-             ("detour" . "OUT: Proxy")
-             ("server" . "1.1.1.1"))))
-       ("rules"
-        . #(,@(map (lambda (rule)
-                     `(,@rule
-                       ("server" . "DNS: Direct")))
-                   %direct-rules)
-            ,@(map (lambda (rule)
-                     `(,@rule
-                       ("server" . "DNS: Proxy")))
-                   %proxy-rules))))
-      ("inbounds"
-       . #((("type" . "direct")
-            ("tag" . "IN: DNS")
-            ("listen" . "0.0.0.0")
-            ("listen_port" . 53)
-            ("network" . "udp"))
-           (("type" . "mixed")
-            ("tag" . "IN: Proxy")
-            ("listen" . "0.0.0.0")
-            ("listen_port" . 7890))
-           (("type" . "tun")
-            ("tag" . "IN: Tun")
-            ("address" . #("172.19.0.1/30" "fd00::1/126"))
-            ("auto_route" . #t)
-            ("auto_redirect" . #t)
-            ("strict_route" . #t))))
-      ("outbounds"
-       . #((("type" . "direct")
-            ("tag" . "OUT: Direct"))
-           (("type" . "block")
-            ("tag" . "OUT: Block"))
-           ,(call-with-input-string (sops-str dorphine.yaml '("sing-box")) read)))
-      ("route"
-       ("rules"
-        . #((("action" . "sniff"))
-            (("protocol" . "dns")
-             ("action" . "hijack-dns"))
-            (("ip_is_private" . #t)
-             ("outbound" . "OUT: Direct"))
-            ,@(map (lambda (rule)
-                     `(,@rule
-                       ("outbound" . "OUT: Block")))
-                   %block-rules)
-            ,@(map (lambda (rule)
-                     `(,@rule
-                       ("outbound" . "OUT: Direct")))
-                   %direct-rules)
-            ,@(map (lambda (rule)
-                     `(,@rule
-                       ("outbound" . "OUT: Proxy")))
-                   %proxy-rules)))
-       ("rule_set" . #(,@%rule-sets))
-       ("auto_detect_interface" . #t)
-       ("default_domain_resolver" . "DNS: Direct"))
-      ("experimental"
-       ("cache_file"
-        ("enabled" . #t)
-        ("store_rdrc" . #t)))))
-
-  (computed-file "sing-box.json"
-    (with-extensions (map specification->package '("guile-json@4"))
-      #~(begin
-          (use-modules (json))
-          (call-with-output-file #$output
-            (lambda (port)
-              (display (scm->json-string '#$%config #:pretty #t) port)))))))
