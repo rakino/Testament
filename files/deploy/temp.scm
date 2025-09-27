dd52bf883482147181115158f619f99df7551c89
Hilton Chain <hako@ultrarare.space>
2025-09-28 01:37:06 +0800

    temp: Support BIOS machines.

--- files/deploy/temp.scm
+++ files/deploy/temp.scm
@@ -17,6 +17,15 @@
            (machine-ssh-configuration
              (host-name ip-address)
              (system system)
+             (host-key ssh-host-key)))))
+       ((ip-address system ssh-host-key max-jobs threads-per-job bios-boot-disk)
+        (machine
+          (operating-system (%os max-jobs threads-per-job bios-boot-disk))
+          (environment managed-host-environment-type)
+          (configuration
+           (machine-ssh-configuration
+             (host-name ip-address)
+             (system system)
              (host-key ssh-host-key))))))
      '(#;("0.0.0.0"
           "x86_64-linux"
