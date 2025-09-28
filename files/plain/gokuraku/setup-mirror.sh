bd43cfd194508e90015d752f8c8207a1f1d25b2e
Hilton Chain <hako@ultrarare.space>
2025-09-28 13:10:46 +0800

    gokuraku: setup-mirror: Output more information.

--- files/plain/gokuraku/setup-mirror.sh
+++ files/plain/gokuraku/setup-mirror.sh
@@ -7,8 +7,11 @@ if [ "x$BASH_VERSION" = "x" ]; then
     exec bash "$0" "$@"
 fi
 
-die()
-{
+msg() {
+    echo "INFO: $*"
+}
+
+die() {
     echo "ERROR: $*"
     exit 1
 }
@@ -21,68 +24,86 @@ RCLONE_ARGS="\
 
 NARINFO_OLD=/var/cache/r2/narinfo-old.txt
 NARINFO_NEW=/var/cache/r2/narinfo-new.txt
-NARINFO_DEL=/var/cache/r2/narinfo-to-delete.txt
-NARINFO_CPY=/var/cache/r2/narinfo-to-copy.txt
+NARINFO_DIFF=/var/cache/r2/nar.diff
+NARINFO_TO_DELETE=/var/cache/r2/narinfo-to-delete.txt
+NARINFO_TO_COPY=/var/cache/r2/narinfo-to-copy.txt
 
 NAR_OLD=/var/cache/r2/nar-old.txt
 NAR_NEW=/var/cache/r2/nar-new.txt
-NAR_DEL=/var/cache/r2/nar-to-delete.txt
-NAR_CPY=/var/cache/r2/nar-to-copy.txt
+NAR_DIFF=/var/cache/r2/narinfo.diff
+NAR_TO_DELETE=/var/cache/r2/nar-to-delete.txt
+NAR_TO_COPY=/var/cache/r2/nar-to-copy.txt
 
 list_narinfo() {
+    msg "listing changes to narinfo files"
+
     pushd /var/cache/guix/publish
     fd '\.narinfo$' --type f | sort > $NARINFO_NEW
     popd
 
-    pushd /var/cache/r2
-    diff --unified $NARINFO_OLD $NARINFO_NEW | tail +4 > narinfo.diff
-    rg '^-'  narinfo.diff --no-line-number --replace '' > $NARINFO_DEL
-    rg '^\+' narinfo.diff --no-line-number --replace '' > $NARINFO_CPY
-    rm --force narinfo.diff
-    popd
+    diff --unified $NARINFO_OLD $NARINFO_NEW | tail +4 > $NARINFO_DIFF
+    rg '^-'  $NARINFO_DIFF --no-line-number --replace '' > $NARINFO_TO_DELETE
+    rg '^\+' $NARINFO_DIFF --no-line-number --replace '' > $NARINFO_TO_COPY
+    rm --force $NARINFO_DIFF
 }
 
 list_nar() {
+    msg "listing changes to nar archives"
+
     pushd /var/cache/guix/publish/nar
     fd --type f --exclude '*.tmp' | sort > $NAR_NEW
     popd
 
-    pushd /var/cache/r2
-    diff --unified $NAR_OLD $NAR_NEW | tail +4 > nar.diff
-    rg '^-'  nar.diff --no-line-number --replace '' > $NAR_DEL
-    rg '^\+' nar.diff --no-line-number --replace '' > $NAR_CPY
-    rm --force nar.diff
-    popd
+    diff --unified $NAR_OLD $NAR_NEW | tail +4 > $NAR_DIFF
+    rg '^-'  $NAR_DIFF --no-line-number --replace '' > $NAR_TO_DELETE
+    rg '^\+' $NAR_DIFF --no-line-number --replace '' > $NAR_TO_COPY
+    rm --force $NAR_DIFF
 }
 
 delete_narinfo() {
+    msg "removing unavailable narinfo files"
+
     pushd /var/cache/guix-moe
     old_IFS=$IFS
     IFS=""
     while read -r file ; do
         rm --force "$file"
-    done < $NARINFO_DEL
+    done < $NARINFO_TO_DELETE
     IFS=${old_IFS}
     popd
 }
 
 copy_narinfo() {
+    msg "copying new narinfo files"
+
     pushd /var/cache/guix/publish
     old_IFS=$IFS
     IFS=""
     while read -r file ; do
         cp --archive --force "$file" /var/cache/guix-moe
-    done < $NARINFO_CPY
+    done < $NARINFO_TO_COPY
     IFS=${old_IFS}
     popd
 }
 
 delete_nar() {
-    [[ -s $NAR_DEL ]] && rclone delete $RCLONE_ARGS --files-from $NAR_DEL r2:substitutes-apac/nar
+    msg "removing unavailable nar archives from Cloudfare R2"
+
+    [[ -s $NAR_TO_DELETE ]] &&
+        rclone delete $RCLONE_ARGS --files-from $NAR_TO_DELETE r2:substitutes-apac/nar
 }
 
 upload_nar() {
-    [[ -s $NAR_CPY ]] && rclone copy   $RCLONE_ARGS --files-from $NAR_CPY /var/cache/guix/publish/nar r2:substitutes-apac/nar
+    msg "uploading new nar archives to Cloudflare R2"
+
+    [[ -s $NAR_TO_COPY ]] &&
+        rclone copy $RCLONE_ARGS --files-from $NAR_TO_COPY /var/cache/guix/publish/nar r2:substitutes-apac/nar
+}
+
+clean_up() {
+    rm $NARINFO_TO_DELETE $NARINFO_TO_COPY $NAR_TO_DELETE $NAR_TO_COPY
+    mv $NARINFO_NEW $NARINFO_OLD
+    mv $NAR_NEW $NAR_OLD
 }
 
 main() {
@@ -94,14 +115,14 @@ main() {
     list_narinfo
     list_nar
 
+    set -eo pipefail
+
     delete_narinfo
     upload_nar
     copy_narinfo
     delete_nar
 
-    rm --force $NARINFO_DEL $NARINFO_CPY $NAR_DEL $NAR_CPY
-    mv --force $NARINFO_NEW $NARINFO_OLD
-    mv --force $NAR_NEW $NAR_OLD
+    clean_up
 }
 
 main
