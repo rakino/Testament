412f6ab89b4e18893e65454f17120aae67c22cb8
Hilton Chain <hako@ultrarare.space>
2025-10-17 15:47:03 +0800

    worker: Add host name configuration.

--- files/deploy/worker.scm
+++ files/deploy/worker.scm
@@ -7,9 +7,9 @@
 
 (define %os (load "../../config/worker.scm"))
 
-(define* (build-worker #:key address system (32bit-support? #t) ssh-host-key workers threads-per-worker (bios-boot #f))
+(define* (build-worker #:key host-name address system (32bit-support? #t) ssh-host-key workers threads-per-worker (bios-boot #f))
   (machine
-    (operating-system (%os system 32bit-support? workers threads-per-worker bios-boot))
+    (operating-system (%os host-name system 32bit-support? workers threads-per-worker bios-boot))
     (environment managed-host-environment-type)
     (configuration
      (machine-ssh-configuration
@@ -17,17 +17,8 @@
        (system system)
        (host-key ssh-host-key)))))
 
-(define (hetzner-worker system address)
-  (build-worker
-   #:address address
-   #:system system
-   #:32bit-support? #t
-   #:ssh-host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIED2WXdbkA7slzknPrzc3QL+fmrU2eaPRENdVxKElVXb root@(none)"
-   #:workers 4
-   #:threads-per-worker 4
-   #:bios-boot (and (string=? "x86_64-linux" system) "/dev/sda")))
-
 (list #;(build-worker
+         #:host-name "..."
          #:address "0.0.0.0"
          #:system "aarch64-linux"
          #:32bit-support? #t
@@ -35,6 +26,7 @@
          #:workers 4
          #:threads-per-worker 2)
       #;(build-worker
+         #:host-name "..."
          #:address "0.0.0.0"
          #:system "x86_64-linux"
          #:32bit-support? #t
