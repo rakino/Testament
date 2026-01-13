9622ec4762c97570221f8c3e6b472d4194c02e1e
Hilton Chain <hako@ultrarare.space>
2026-01-13 23:51:38 +0800

    Add REUSE.toml.

--- channels.scm
+++ channels.scm
@@ -1,6 +1,6 @@
 ;;; SPDX-FileCopyrightText: 2025 Hilton Chain <hako@ultrarare.space>
 ;;;
-;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; SPDX-License-Identifier: CC0-1.0
 
 (cons* (channel
          (name 'nonguix)
