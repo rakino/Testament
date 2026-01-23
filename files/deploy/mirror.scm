0989e10b2d7b0136402b7a1e09acc0ae765892fb
Hilton Chain <hako@ultrarare.space>
2026-01-23 21:06:49 +0800

    mirror: Upgrade cache-sg and cache-us-lax.

--- files/deploy/mirror.scm
+++ files/deploy/mirror.scm
@@ -7,23 +7,25 @@
 
 (define %os (load "../../config/mirror.scm"))
 
-(define* (mirror #:key mirror-name system ssh-host-key (bios-boot #f))
+(define* (mirror #:key mirror-name host-name system ssh-host-key (bios-boot #f))
   (machine
     (operating-system (%os mirror-name bios-boot))
     (environment managed-host-environment-type)
     (configuration
      (machine-ssh-configuration
-       (host-name (string-append mirror-name ".guix.moe"))
+       (host-name
+        (or host-name
+            (string-append mirror-name ".guix.moe")))
        (system system)
        (host-key ssh-host-key)))))
 
 (list (mirror
        #:mirror-name "cache-sg"
        #:system "x86_64-linux"
-       #:ssh-host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9Ad1hMSRW2+mP8UTx6XCK8ioLA+1ZyBmCHChvvBvzk"
+       #:ssh-host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7LUCU7btbuvWNMvS3WnM6lAZLB8AwH/O9LdYhae9Eo"
        #:bios-boot "/dev/vda")
       (mirror
        #:mirror-name "cache-us-lax"
        #:system "x86_64-linux"
-       #:ssh-host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE3MpODvl8Ifk8tShMzMfourJELup+bwJLOYL+cYTWBP"
+       #:ssh-host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIymKc9HG2Gr+4r2mG3zVdRsCewZ9WuVrOJZipbMWHrl"
        #:bios-boot "/dev/vda"))
