f7cfda0ca89eabf7fa9f9fc404562d338b89c1e5
Hilton Chain <hako@ultrarare.space>
2026-02-02 12:45:30 +0800

    nuporta: setup-mirror: Adjust nars list creation and cleaning-up.

--- files/plain/setup-mirror.scm
+++ files/plain/setup-mirror.scm
@@ -24,43 +24,40 @@
           (in-vicinity mirror-cache "narinfo/")))
 
 ;; Reduce S3 operations.
+;; A bit hacky but it works and takes least time to think and write.
+(define %nars-list      "nars.txt")
+(define %nars-list-new  (string-append %nars-list ".new"))
+(define %nars-diff      (string-append %nars-list ".diff"))
+(define %nars-to-copy   (string-append %nars-list ".to_copy"))
+(define %nars-to-delete (string-append %nars-list ".to_delete"))
+
 (define (list-changed-nars mirror-cache publish-cache)
-  (let* ((file "nars.txt")
-         (new  (string-append file ".new"))
-         (diff (string-append file ".diff"))
-         (to-copy   (string-append file ".to_copy"))
-         (to-delete (string-append file ".to_delete")))
-    (unless (file-exists? file)
-      (call-with-output-file file (const #t)))
-    (with-directory-excursion (in-vicinity publish-cache "nar")
-      (invoke* "fd" "--exclude" "'*.tmp'" "--type" "f" "|" "sort" ">" (in-vicinity mirror-cache new)))
-    (with-directory-excursion mirror-cache
-      (invoke* "diff" "--unified" file new "|" "tail" "+4" ">" diff)
-      (false-if-exception
-       (invoke* "rg" "'^\\+'" diff "--no-line-number" "--replace" "''" ">" to-copy))
-      (false-if-exception
-       (invoke* "rg" "'^-'"   diff "--no-line-number" "--replace" "''" ">" to-delete)))))
+  (with-directory-excursion (in-vicinity publish-cache "nar")
+    (invoke* "fd" "--exclude" "'*.tmp'" "--type" "f" "|" "sort" ">" (in-vicinity mirror-cache %nars-list-new)))
+  (with-directory-excursion mirror-cache
+    (unless (file-exists? %nars-list)
+      (call-with-output-file %nars-list (const #t)))
+    (invoke* "diff" "--unified" %nars-list %nars-list-new "|" "tail" "+4" ">" %nars-diff)
+    (false-if-exception
+     (invoke* "rg" "'^\\+'" %nars-diff "--no-line-number" "--replace" "''" ">" %nars-to-copy))
+    (false-if-exception
+     (invoke* "rg" "'^-'"   %nars-diff "--no-line-number" "--replace" "''" ">" %nars-to-delete))
+    (delete-file* %nars-diff)))
 
 (define (upload-new-nars mirror-cache publish-cache rclone-s3-bucket)
   (with-directory-excursion mirror-cache
-    (apply invoke "rclone" "copy" "--files-from" "nars.txt.to_copy"
+    (apply invoke "rclone" "copy" "--files-from" %nars-to-copy
            (in-vicinity publish-cache "nar")
            (in-vicinity rclone-s3-bucket "nar")
-           %rclone-arguments)))
+           %rclone-arguments)
+    (delete-file* %nars-to-copy)))
 
 (define (delete-expired-nars mirror-cache rclone-s3-bucket)
   (with-directory-excursion mirror-cache
-    (apply invoke "rclone" "delete" "--files-from" "nars.txt.to_delete"
+    (apply invoke "rclone" "delete" "--files-from" %nars-to-delete
            (in-vicinity rclone-s3-bucket "nar")
-           %rclone-arguments)))
-
-(define (clean-up mirror-cache)
-  (with-directory-excursion mirror-cache
-    (for-each delete-file*
-              '("nars.txt.diff"
-                "nars.txt.to_copy"
-                "nars.txt.to_delete"))
-    (rename-file "nars.txt.new" "nars.txt")))
+           %rclone-arguments)
+    (delete-file* %nars-to-delete)))
 
 (with-file-lock/no-wait "/var/lock/guix-moe-mirror.lock"
   (lambda _
@@ -68,8 +65,11 @@
   (begin
     (setenv "PATH" "/run/current-system/profile/bin")
     (mkdir-p %mirror-cache)
+
     (list-changed-nars   %mirror-cache %publish-cache)
     (upload-new-nars     %mirror-cache %publish-cache "r2:substitutes-apac")
     (sync-narinfo        %mirror-cache %publish-cache)
     (delete-expired-nars %mirror-cache "r2:substitutes-apac")
-    (clean-up %mirror-cache)))
+
+    (with-directory-excursion %mirror-cache
+      (rename-file %nars-list-new %nars-list))))
